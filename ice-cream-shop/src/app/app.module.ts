245ad2cd6c88fcbce43f40bdb33bdfc4d7f068c0
Seth Arvila <setharvila@hotmail.com>
2023-05-02 15:05:20 -0500

    homeComplete

--- ice-cream-shop/src/app/app.module.ts
+++ ice-cream-shop/src/app/app.module.ts
@@ -5,12 +5,11 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-
-    
   ],
   imports: [
     BrowserModule
