b9518b3e654ec353f63a0b92ddd843c698854d79
samuellucht <125292545+samuellucht@users.noreply.github.com>
2023-05-04 22:12:11 -0500

    login component and canLogin method

--- ice-cream-shop/src/app/components/login/login.component.ts
+++ ice-cream-shop/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { AccountService } from 'src/app/services/account.service'; 
+import {Account} from 'src/app/model';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  constructor(private accountService: AccountService){}
+
+  inputEmail: string = '';
+  inputPassword: string = '';
+
+  id: number = 0;
+  firstName: string = '';
+  lastName: string = '';
+  phone: number = 0;
+  emailAddress: string = '';
+  password: string = '';
+  isEmployee: boolean = false;
+  isManager: boolean = false;
+
+  addNewAccount(){
+    this.accountService.addAccount(this.id, this.firstName, this.lastName, this.phone, this.emailAddress, this.password, this.isEmployee, this.isManager);
+  }
+
+  removeAccount(){
+    this.accountService.deleteAccount(this.id);
+  }
+
+  canLogin(){
+    this.accountService.checkLogin(this.inputEmail, this.inputPassword);
+  }
 
 }
